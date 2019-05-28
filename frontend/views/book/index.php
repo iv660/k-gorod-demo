fe5652a86be44fdedc546e0508cd27a4d25d9576
dw.ilya <info@docwriter.ru>
2019-05-28 13:13:53 +0300

    Fix: - replace the hard-coded API URL with a dynamic one.

--- frontend/views/book/index.php
+++ frontend/views/book/index.php
@@ -2,6 +2,7 @@
 
 use frontend\assets\BooksJsxAsset;
 use yii\web\View;
+use yii\helpers\URL;
 
 /* @var $this yii\web\View */
 
@@ -17,9 +18,17 @@ $this->title = Yii::t('app', 'Books Directory');
         <div id="books-container"><?= Yii::t('app', 'Loading...') ?></div>
     </div>
 </div>
+
 <?php 
+// Build API URL
+$apiUrl = Yii::$app->urlManager->createAbsoluteUrl('');
+$rootDir = '/frontend/';
+$rootPosition = strpos($apiUrl, $rootDir);
+$rootLength = strlen($rootDir);
+$apiUrl = substr($apiUrl, 0, $rootPosition) . '/api/web/' . Url::to('api/v1/books');
+
 $this->registerJs(<<<JS
-    booksConfig = {api_url: 'https://php7.docwriter.ru/k-gorod/api/web/api/v1/books'};
+    booksConfig = {api_url: '{$apiUrl}'};
 JS
     ,
     View::POS_HEAD);
